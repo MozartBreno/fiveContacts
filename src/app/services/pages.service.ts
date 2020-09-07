a2143b2aa601cf9889eb04006f57dc003b8a10e1
Mozart Breno <MozartBreno@Air-de-deway.lan>
2020-09-07 20:53:10 -0300

    first commit

---
+++ src/app/services/pages.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PagesService {
+
+  constructor() { }
+
+  getPages() {
+    return [
+      {
+        title: 'Home',
+        url: '/home',
+        icon: 'home'
+      },
+      {
+        title: 'Favoritos',
+        url: '/favorite',
+        icon: 'heart'
+      }
+    ];
+  }
+}
